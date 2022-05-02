e23ed6099453db222555d09d06826ec8e7be4797
Casres <christian@casillas.tech>
2022-05-02 03:58:12 -0400

    added the CLI prompts for all roles

---
+++ index.js
@@ -0,0 +1,136 @@
+
+
+
+
+
+
+
+
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+const HTMLTemplate = require("./src/HTMLtemplate");
+
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+const teamArr = [];
+
+init();
+
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: "confirm",
+        name: "position",
+        message: "Would you like to fill a position?",
+      },
+    ])
+    .then(({ position }) => {
+      position ? fillPosition() : createHtml();
+    });
+}
+
+function fillPosition() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'roles',
+            message: 'What job would you like to fill?',
+            choices: ['Manager', 'Engineer', 'Intern']
+        }
+    ])
+    // asking the employee constructor questions to the user
+    .then(({roles}) => {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the employee\'s name?',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter a name.');
+                        return false;
+                    }
+                }
+            },
+
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the employee\'s ID # ?',
+                validate: idInput => {
+                    if (idInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter an ID #.');
+                        return false;
+                    }
+                }
+            },
+
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is the employee\'s email?',
+                validate: emailInput => {
+                    if (emailInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter an email');
+                        return false;
+                    }
+                }
+            },
+
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: 'What is the office number of the Manager?',
+                validate: officeNumberInput => {
+                    if (officeNumberInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter an office number');
+                        return false;
+                    }
+                },
+                when: roles == 'Manager'
+            },
+
+            {
+                type: 'input',
+                name: 'GitHub',
+                message: 'What is the engineers GitHub?',
+                validate: GitHubInput => {
+                    if (GitHubInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the engineers GitHub');
+                        return false;
+                    }
+                },
+                when: roles == 'Engineer'
+            },
+
+            {
+                type: 'input',
+                name: 'School',
+                message: 'What is the interns school?',
+                validate: SchoolInput => {
+                    if (SchoolInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter the interns school');
+                        return false;
+                    }
+                },
+                when: roles == 'Intern'
+            }
+        ])
+    })
+};
--- src/HTMLtemplate.js
+++ src/HTMLtemplate.js
@@ -2,8 +2,82 @@
 
 
 
+const generateTeam = team => {
+    const generateManager = (manager) => {
+        return `
+        <div class="card" style="width: 18rem;">
+            <div class="card-header">
+                <h2>Name</h2>
+                <h3> <i class="fa-solid fa-briefcase"></i> Manager </h3>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: </li>
+                <li class="list-group-item">Email: </li>
+                <li class="list-group-item">Office Number: </li>
+            </ul>
+        </div>
+        `
+    };
+
+    const generateEngineer = (engineer) => {
+        return `
+        <div class="card" style="width: 18rem;">
+            <div class="card-header">
+                <h2>Name</h2>
+                <h3> <i class="fa-solid fa-calculator"></i> Engineer </h3>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: </li>
+                <li class="list-group-item">Email: </li>
+                <li class="list-group-item">GitHub: </li>
+            </ul>
+        </div>
+        `
+    };
+
+    const generateIntern = (intern) => {
+        return `
+        <div class="card" style="width: 18rem;">
+            <div class="card-header">
+                <h2>Name</h2>
+                <h3> <i class="fa-solid fa-mug-hot"></i> Intern</h3>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: </li>
+                <li class="list-group-item">Email: </li>
+                <li class="list-group-item">School: </li>
+            </ul>
+        </div>
+        `
+    };
+}
 
 
+module.exports = team => {
+    ` 
+        <!DOCTYPE html>
+        <html lang="en">
 
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" />
+            <link rel="stylesheet" href="./style.css">
+            <title>Document</title>
+        </head>
 
+        <body>
+            <h1 id="title" class="d-flex justify-content-center mt-0">My Team</h1>
+
+            <div class="row d-flex justify-content-around ">
+                ${generateTeam(team)}
+            </div>
+
+        <script src="https://kit.fontawesome.com/da5306b6da.js" crossorigin="anonymous"></script>
+        </body>
+
+        </html>
+    `
+}
 
